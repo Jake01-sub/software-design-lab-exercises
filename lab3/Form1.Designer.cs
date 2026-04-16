3f1db70767ba1232e441278b4d33edf6aad0b340
Jake01-sub <l.buchan.476132@umindanao.edu.ph>
2026-04-16 15:02:02 +0800

    Week 3 Complete

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             btnShowInfo = new Button();
+            lstBookTypes = new ListBox();
+            btnShowAll = new Button();
             SuspendLayout();
             // 
             // btnShowInfo
             // 
+            btnShowInfo.Anchor = AnchorStyles.None;
             btnShowInfo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnShowInfo.Location = new Point(337, 199);
+            btnShowInfo.Location = new Point(70, 158);
             btnShowInfo.Name = "btnShowInfo";
             btnShowInfo.Size = new Size(127, 52);
             btnShowInfo.TabIndex = 0;
@@ -42,21 +45,46 @@
             btnShowInfo.UseVisualStyleBackColor = true;
             btnShowInfo.Click += btnShowInfo_Click;
             // 
+            // lstBookTypes
+            // 
+            lstBookTypes.FormattingEnabled = true;
+            lstBookTypes.ItemHeight = 15;
+            lstBookTypes.Location = new Point(266, 51);
+            lstBookTypes.Name = "lstBookTypes";
+            lstBookTypes.Size = new Size(477, 349);
+            lstBookTypes.TabIndex = 1;
+            // 
+            // btnShowAll
+            // 
+            btnShowAll.Anchor = AnchorStyles.None;
+            btnShowAll.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnShowAll.Location = new Point(70, 240);
+            btnShowAll.Name = "btnShowAll";
+            btnShowAll.Size = new Size(127, 52);
+            btnShowAll.TabIndex = 2;
+            btnShowAll.Text = "Show All";
+            btnShowAll.UseVisualStyleBackColor = true;
+            btnShowAll.Click += btnShowAll_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnShowAll);
+            Controls.Add(lstBookTypes);
             Controls.Add(btnShowInfo);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
+            Text = "Bookstore System";
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button btnShowInfo;
+        private ListBox lstBookTypes;
+        private Button btnShowAll;
     }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -28,6 +28,47 @@ namespace lab3
             }
         }
 
+        public class Ebook : Book
+        {
+            public double FileSizeMB { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} - File Size MB: {FileSizeMB}";
+            }
+
+        }
+        public class Textbook : Book
+        {
+            public string Subject { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} [Subject: {Subject}]";
+            }
+        }
+
+        public class AudioBook : Book
+        {
+            public double Duration { get; set; }
+            public string Narrator { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} (Narrated by {Narrator}, {Duration} hrs)";
+            }
+        }
+
+        private void ProcessBooks(Book[] bookstoreInventory)
+        {
+            lstBookTypes.Items.Clear();
+
+            foreach (Book item in bookstoreInventory)
+            {
+                lstBookTypes.Items.Add(item.GetInfo());
+            }
+        }
+
         private void btnShowInfo_Click(object sender, EventArgs e)
         {
             Book myBook = new Magazine
@@ -38,6 +79,49 @@ namespace lab3
             };
 
             MessageBox.Show(myBook.GetInfo());
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            Book[] myInventory = new Book[]
+            {
+                new Book
+                {
+                    Title = "Rage of Angels",
+                    Author = "Sidney Sheldon"
+                },
+
+                new Magazine
+                {
+                    Title = "Tech Weekly",
+                    Author = "John Doe",
+                    IssueNumber = 45
+                },
+
+                new Ebook
+                {
+                    Title = "C# Programming Guide",
+                    Author = "Bob Johnson",
+                    FileSizeMB = 5.2
+                },
+
+                new Textbook
+                {
+                    Title = "Introduction to Algorithms",
+                    Author = "Thomas H. Cormen",
+                    Subject = "Computer Science"
+                },
+
+                new AudioBook
+                {
+                    Title = "Project Hail Mary",
+                    Author = "Andy Weir",
+                    Narrator = "Ray Porter",
+                    Duration = 16.2
+                }
+            };
+
+            ProcessBooks(myInventory);
         }
     }
 }
